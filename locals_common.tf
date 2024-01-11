d9285abc09ef98ccf2ce1b5ff6f66ce139a6fa9e
JaySon-Huang <tshent@qq.com>
2024-01-11 16:27:51 +0800

    save
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- locals_common.tf
+++ locals_common.tf
@@ -1,8 +1,8 @@
 locals {
-  namespace         = "wenxuan-demo"
-  n_tidb            = 2
-  n_tikv            = 3
-  n_tiflash_write   = 2
-  n_tiflash_compute = 3
+  namespace         = "jayson-test"
+  n_tidb            = 1
+  n_tikv            = 8
+  n_tiflash_write   = 1
+  n_tiflash_compute = 1
   username          = "ubuntu"
 }

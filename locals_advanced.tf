4628a69321dddda824f60b426fb1ff8d0f7f4b20
JaySon-Huang <tshent@qq.com>
2025-08-28 15:56:19 +0800

    Add amd rockylinux9
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- locals_advanced.tf
+++ locals_advanced.tf
@@ -4,10 +4,11 @@ locals {
   # image is region-local. If you changed region, please also change image.
   region = "us-west-2"
   # https://rockylinux.org/download
-  image = "ami-03be04a3da3a40226" # Rocky-9-EC2-Base-9.5-20241118.0.aarch64
+  image = "ami-0fadb4bc4d6071e9e" # Rocky-9-EC2-Base-9.6-20250531.0.x86_64
 
-  # # If you want to change instance type, ensure that GP3 EBS is available in the instance type.
-  center_instance  = "m6g.2xlarge"
+  # If you want to change instance type, ensure that GP3 EBS is available in the instance type.
+  # https://aws.amazon.com/ec2/instance-types/
+  center_instance  = "m7i.2xlarge"
 
   master_ssh_key         = "./master_key"
   master_ssh_public      = "./master_key.pub"
--- locals_common.tf
+++ locals_common.tf
@@ -1,4 +1,4 @@
 locals {
-  namespace         = "jayson-arm-build"
+  namespace         = "jayson-amd-build"
   username          = "rocky"
 }
